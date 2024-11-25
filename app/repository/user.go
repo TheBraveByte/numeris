f40b527950e5a64eeb4eb3dac1fea9cea3f218c4
TheBraveByte <ayaaakinleye@gmail.com>
2024-11-25 21:58:58 +0100

    feat : user interface implements methods

--- app/repository/user.go
+++ app/repository/user.go
@@ -1,3 +1,14 @@
 package repository
 
-type
+import (
+	"go.mongodb.org/mongo-driver/mongo"
+
+	"github.com/thebravebyte/numeris/domain"
+)
+
+type UserRepository interface {
+	AddUser(db *mongo.Client, user *domain.User, email string) (interface{}, *domain.User)
+	VerifyLogin(db *mongo.Client, email, password string) (*domain.User, error)
+	SaveToken(db *mongo.Client, id string, accessToken string) error
+	UpdatePassword(db *mongo.Client, email, password string) error
+}
