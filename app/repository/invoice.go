9d9ae4405692a2307c921ac9c5a9f290f2326b5b
TheBraveByte <ayaaakinleye@gmail.com>
2024-11-29 04:26:35 +0100

    feat : added queries methods to implement an interface

--- app/repository/invoice.go
+++ app/repository/invoice.go
@@ -7,5 +7,16 @@ import (
 )
 
 type InvoiceRepository interface {
-	AddNewInvoice(db *mongo.Client, invoice *domain.Invoice) interface{}
+	AddNewInvoice(db *mongo.Client, userID string, invoice *domain.Invoice) error
+	FindUserInvoiceByID(db *mongo.Client, userID, invoiceID string) (*domain.Invoice, error)
+	FindAllInvoice(db *mongo.Client, userID string) ([]*domain.Invoice, error)
+	InvoiceStatSummary(db *mongo.Client, userID string) (*domain.InvoiceSummary, error)
+	InvoiceItemSummary(db *mongo.Client, userID string, invoiceID string) ([]domain.Item, error)
+	
+	UpdateInvoiceBeforeDueDate(db *mongo.Client, userID string, invoiceID string, updatedInvoice *domain.Invoice) error
+	GetIssueInvoiceList(db *mongo.Client, userID string) ([]domain.Invoice, error) 
+	UpdateInvoiceStatusToIssued(db *mongo.Client, userID string, invoiceID string) error
+	
+
+	DeleteInvoice(db *mongo.Client, userID, invoiceID string) error
 }
